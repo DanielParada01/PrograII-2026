f21dfd18bf2adb1ed66161da780e941a1ded654f
Daniel Parada <carlosdanielparadaavalos20012@gmail.com>
2026-02-25 12:51:50 -0800

    creacion de la rama conversores

--- miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
+++ miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
@@ -13,75 +13,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-/*
-Tarea para viernes 06/02/2026 individual.
-Factorial
-Porcentaje
-Exponenciacionn
-Raiz
-* */
-//probando primer commit.
+
 public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
-    Button btn;
-    Spinner spn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v->calcular());
-    }
-    private void calcular(){
-        tempVal = findViewById(R.id.txtNum1);
-        Double num1 = Double.parseDouble(tempVal.getText().toString());
-
-        tempVal = findViewById(R.id.txtNum2);
-        Double num2 = Double.parseDouble(tempVal.getText().toString());
-
-        double respuesta = 0;
-
-        spn = findViewById(R.id.cboOpciones);
-        switch (spn.getSelectedItemPosition()){
-            case 0: //suma
-                respuesta = num1 + num2;
-                break;
-
-            case 1: //Resta
-                respuesta = num1 - num2;
-                break;
-
-            case 2: //Multiplicacion
-                respuesta = num1 * num2;
-                break;
-
-            case 3: //division
-                respuesta = num1 / num2;
-                break;
-
-            case 4: // Porcentaje
-                respuesta = (num1 * num2) / 100;
-                break;
-
-            case 5: // Factorial
-                long fact = 1;
-                for(int i = 1; i <= num1; i++){
-                    fact *= i;
-                }
-                respuesta = fact;
-                break;
-
-            case 6: // Exponenciacion
-                respuesta = Math.pow(num1, num2);
-                break;
-
-            case 7: // Raiz cuadrada
-                respuesta = Math.sqrt(num1);
-                break;
-
-        }
-        tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta);
     }
 }
